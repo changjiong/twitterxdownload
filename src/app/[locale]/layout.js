47f3b1ae92a8ec22f60fc02f565443a78b381f51
changjiong <drunkwretch@gmail.com>
2025-06-12 19:04:59 +0800

    fix: 在layout.js中添加Google Adsense脚本

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -53,6 +53,8 @@ export default function RootLayout({ children, params }) {
     return (
       <html lang={locale} suppressHydrationWarning>
         <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9756036541068145"
+     crossorigin="anonymous"></script>
             <GoogleAdsense />
             <GoogleAnalytics />
         </head>
